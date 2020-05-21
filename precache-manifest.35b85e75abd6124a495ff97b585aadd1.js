066ad046ce73457a02bb8b5121d286126c0fa8c5
dev <devsingh6n@gmail.com>
2020-05-21 22:06:51 +0530

    Updates

---
+++ precache-manifest.35b85e75abd6124a495ff97b585aadd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cce48f62bc42d6d19689c01bb4a61358",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5a1fc3e27cda37f74931",
+    "url": "/monsters-rolodex/static/css/main.fa3b95ca.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a1fc3e27cda37f74931",
+    "url": "/monsters-rolodex/static/js/main.e7a97434.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
